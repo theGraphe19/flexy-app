04348a68cd2ef27d2a69aa8660ce331e495b1232
rishavk1102 <rishavk1102@gmail.com>
2020-05-25 23:50:51 +0530

    Bug Fixes 4 - Mapping to changed API

--- lib/credentials.dart
+++ lib/credentials.dart
@@ -1,15 +1,15 @@
 const MSG91_API_KEY = "314319Asz8t1bwU0qU5e27d970P1";
-const registerUrl = "https://developers.thegraphe.com/flexy/public/reg";
-const loginUrl = "https://developers.thegraphe.com/flexy/public/login";
-const logOutUrl = "https://developers.thegraphe.com/flexy/public/logout?api_token=";
+const registerUrl = "https://developers.thegraphe.com/flexy/reg";
+const loginUrl = "https://developers.thegraphe.com/flexy/login";
+const logOutUrl = "https://developers.thegraphe.com/flexy/logout?api_token=";
 const sendOTPUrl = "https://developers.thegraphe.com/flexy/sendOTP";
 const verifyOTPUrl =
     "https://api.msg91.com/api/v5/otp/verify?authkey=$MSG91_API_KEY&sender=FLEXYM&";
 const getProductsListUrl =
-    "https://developers.thegraphe.com/flexy/public/neworder?api_token=";
-const getProductDetailsUrl = "https://developers.thegraphe.com/flexy/public/proddetails/";
-const placeorderUrl = "https://developers.thegraphe.com/flexy/public/placeorder";   
-const addRemarkUrl = "https://developers.thegraphe.com/flexy/public/addremarks"; 
-const getMyOrdersUrl = "https://developers.thegraphe.com/flexy/public/myorders?api_token=";
+    "https://developers.thegraphe.com/flexy/neworder?api_token=";
+const getProductDetailsUrl = "https://developers.thegraphe.com/flexy/proddetails/";
+const placeorderUrl = "https://developers.thegraphe.com/flexy/placeorder";   
+const addRemarkUrl = "https://developers.thegraphe.com/flexy/addremarks"; 
+const getMyOrdersUrl = "https://developers.thegraphe.com/flexy/myorders?api_token=";
 
 const productImagesURL = 'https://developers.thegraphe.com/flexy/storage/app/product_images/';
