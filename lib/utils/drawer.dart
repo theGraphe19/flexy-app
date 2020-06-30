8d44f7a924595d842887004c80fbc62e7c20ec9c
rishavk1102 <rishavk1102@gmail.com>
2020-07-01 01:31:01 +0530

    Skeleton drawer and linking

---
+++ lib/utils/drawer.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class SideDrawer {
+  Widget drawer() => Drawer(
+        child: Column(
+          children: <Widget>[
+            Text('item1'),
+            Text('item1'),
+            Text('item1'),
+          ],
+        ),
+      );
+}
