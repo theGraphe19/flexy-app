1cf3e3722002197a8568279368bb2eb3b76b2ff7
Sampatlal Jangid <sampatlal.jangid18@vit.edu>
2020-07-05 09:24:59 +0530

    Cart Product Image - Done

--- lib/widgets/cart_item.dart
+++ lib/widgets/cart_item.dart
@@ -16,8 +16,8 @@ class CartItem extends StatelessWidget {
   String token;
   final GlobalKey<ScaffoldState> scaffoldKey;
   ProductDetails productDetails;
-  CartItem(this.item, this._parent, this.index, this.token, this
-  .scaffoldKey);
+
+  CartItem(this.item, this._parent, this.index, this.token, this.scaffoldKey);
 
   Future<String> _getToken() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
@@ -35,77 +35,89 @@ class CartItem extends StatelessWidget {
           child: Column(
             children: <Widget>[
               Container(
-                  child: Row(children: [
-                Image(
-                    image: AssetImage('assets/icon/icon.png'),
-                    fit: BoxFit.cover,
-                    height: 75.0,
-                    width: 75.0),
-                SizedBox(width: 10.0),
-                Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-                  Text(item.productName,
-                      style: TextStyle(
-                          fontFamily: 'Montserrat',
-                          fontSize: 17.0,
-                          fontWeight: FontWeight.bold)),
-                  Text("x${item.quantity}        Size : ${item.productSize}      ID : ${item.id} + ${item.productId}",
-                      style: TextStyle(
-                          fontFamily: 'Montserrat',
-                          fontSize: 15.0,
-                          color: Colors.grey))
-                ])
-              ])),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  IconButton(
-                    icon: Icon(Icons.update),
-                    color: Colors.black,
-                    onPressed: () {
-                      print("Update Order");
-                      print(item.id);
-                      HTTPHandler().getProductDetails(item.productId, token).then((value) {
-                        productDetails = value;
-                        print(productDetails.product.name);
-                        var colorList = new List<List<String>>();
-                        var qtyList = new List<List<int>>();
-                        for (ProductSize productSize in productDetails.product.productSizes) {
-                          var temp1 = new List<String>();
-                          var temp2 = new List<int>();
-                          for (ProductColor productColor in productSize.colors) {
-                            if (!temp1.contains(productColor.color)) {
-                              temp1.add(productColor.color);
-                              temp2.add(productColor.quantity);
-                            }
-                          }
-                          colorList.add(temp1);
-                          qtyList.add(temp2);
-                        }
-                        CartBottomSheet().showBottomSheet(
-                            context,
-                            productDetails.product,
-                            scaffoldKey,
-                            colorList,
-                            qtyList,
-                            token,
-                            true
-                        );
-                      });
-                    },
-                  ),
-                  IconButton(
-                    icon: Icon(Icons.delete),
-                    color: Colors.black,
-                    onPressed: () {
-                      _parent.setState(() {
-                        _parent.items.removeAt(index);
-                        _getToken().then((value) {
-                          HTTPHandler().removeFromCart(value, item.id.toString());
-                        });
-                      });
-                    },
-                  ),
-                ],
+                child: Row(children: [
+                  Image.network(
+                      'https://developers.thegraphe.com/flexy/storage/app/product_images/${item.productImages[0]}',
+                      fit: BoxFit.fill,
+                      height: 120.0,
+                      width: 90.0),
+                  SizedBox(width: 10.0),
+                  Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          item.productName,
+                          style: TextStyle(
+                              fontFamily: 'Montserrat',
+                              fontSize: 17.0,
+                              fontWeight: FontWeight.bold),
+                        ),
+                        Text(
+                          "x${item.quantity}",
+                          style: TextStyle(
+                              fontFamily: 'Montserrat',
+                              fontSize: 15.0,
+                              color: Colors.grey),
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            IconButton(
+                              icon: Icon(Icons.update),
+                              color: Colors.black,
+                              onPressed: () {
+                                print("Update Order");
+                                print(item.id);
+                                HTTPHandler()
+                                    .getProductDetails(item.productId, token)
+                                    .then((value) {
+                                  productDetails = value;
+                                  print(productDetails.product.name);
+                                  var colorList = new List<List<String>>();
+                                  var qtyList = new List<List<int>>();
+                                  for (ProductSize productSize
+                                      in productDetails.product.productSizes) {
+                                    var temp1 = new List<String>();
+                                    var temp2 = new List<int>();
+                                    for (ProductColor productColor
+                                        in productSize.colors) {
+                                      if (!temp1.contains(productColor.color)) {
+                                        temp1.add(productColor.color);
+                                        temp2.add(productColor.quantity);
+                                      }
+                                    }
+                                    colorList.add(temp1);
+                                    qtyList.add(temp2);
+                                  }
+                                  CartBottomSheet().showBottomSheet(
+                                      context,
+                                      productDetails.product,
+                                      scaffoldKey,
+                                      colorList,
+                                      qtyList,
+                                      token,
+                                      true);
+                                });
+                              },
+                            ),
+                            IconButton(
+                              icon: Icon(Icons.delete),
+                              color: Colors.black,
+                              onPressed: () {
+                                _parent.setState(() {
+                                  _parent.items.removeAt(index);
+                                  _getToken().then((value) {
+                                    HTTPHandler().removeFromCart(
+                                        value, item.id.toString());
+                                  });
+                                });
+                              },
+                            ),
+                          ],
+                        ),
+                      ])
+                ]),
               ),
               Divider(),
             ],
